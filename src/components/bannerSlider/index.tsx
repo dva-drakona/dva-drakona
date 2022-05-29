1109efcc3163da85f5cc6b33736d93fed342276b
oleh-domshynskyi <olegkp4@gmail.com>
2022-05-29 03:48:56 +0300

    fix: fix error

--- src/components/bannerSlider/index.tsx
+++ src/components/bannerSlider/index.tsx
@@ -15,11 +15,11 @@ const BannerSlider = ({ data }: BannerSliderProps) => {
     autoplaySpeed: 8000,
   };
   return (
-    <div>
+    <Slider {...settings}>
       {data.images.map((el: any, i: number) => (
         <Image src={el.img} key={i} width={1090} height={500} alt="banner" />
       ))}
-    </div>
+    </Slider>
   );
 };
 
