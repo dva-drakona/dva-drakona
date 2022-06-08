b764b32e5f18438d529bf7c9ee17bd3125571242
oleh-domshynskyi <olegkp4@gmail.com>
2022-06-08 03:14:51 +0300

    fix: add pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux';
 import store from '../redux/store';
 import Preloader from '@/components/preloader';
 import classNames from 'classnames';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [loading, setLoading] = useState(true);
@@ -17,6 +18,33 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   }, []);
   return (
     <Provider store={store}>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Description" />
+        <meta name="keywords" content="Keywords" />
+        <title>Dva Drakona</title>
+
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#317EFB" />
+      </Head>
       <Preloader loading={loading} />
       <div className={classNames(loading ? `opacity-0` : `opacity-100`)}>
         <Component {...pageProps} />
