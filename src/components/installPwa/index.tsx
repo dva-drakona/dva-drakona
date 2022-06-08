093494b4138476ffff8d7803da726f67a1bc96d9
oleh-domshynskyi <olegkp4@gmail.com>
2022-06-08 05:41:53 +0300

    fix: cookie

--- src/components/installPwa/index.tsx
+++ src/components/installPwa/index.tsx
@@ -7,6 +7,23 @@ const InstallPwa = () => {
   const [supportsPWA, setSupportsPWA] = useState<boolean>(true);
   const [promptInstall, setPromptInstall] = useState<any>(null);
   const [isIOS, setIsIOS] = useState<boolean>();
+  const [isAppInstalled, setIsAppInstalled] = useState<any>();
+
+  function getCookie(cname: any) {
+    const name = cname + `=`;
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(`;`);
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ` `) {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return ``;
+  }
 
   useEffect(() => {
     const handler = (e: any) => {
@@ -45,6 +62,7 @@ const InstallPwa = () => {
       }
     }
     setIsIOS(isThisDeviceRunningiOS());
+    setIsAppInstalled(getCookie(`appinstalled`));
 
     return () => window.removeEventListener(`transitionend`, handler);
   }, []);
